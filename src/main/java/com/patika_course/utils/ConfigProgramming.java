c7bb6bd5555ea4773ff7ffd29dd1540ebd31a69e
GHAYETH AL MASRI <amalj6570@gmail.com>
2023-05-22 11:16:16 +0300

    Activating DevTools

---
+++ src/main/java/com/patika_course/utils/ConfigProgramming.java
@@ -0,0 +1,11 @@
+package com.patika_course.utils;
+
+public class ConfigProgramming {
+    /*
+    Open Settings shortcut (ctrl+alt+s)
+     Devtools ==> LiveReload
+     File ==> Settings ==> compiler ==> Build project automatically (ctrl+alt+s)
+     file ==> Advanced settings ==> allow auto-make to even...(ok)
+
+     */
+}
