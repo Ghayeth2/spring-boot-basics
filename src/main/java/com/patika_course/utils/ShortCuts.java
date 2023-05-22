25dfd3eb5633ed8312ac91881882bb42c0f9b999
GHAYETH AL MASRI <amalj6570@gmail.com>
2023-05-22 11:16:36 +0300

    ShortCuts

---
+++ src/main/java/com/patika_course/utils/ShortCuts.java
@@ -0,0 +1,11 @@
+package com.patika_course.utils;
+
+public class ShortCuts {
+    /*
+    open settings (ctrl+alt+s)
+    doubling selected (ctrl+d)
+    constructors, getters & setters, equals & hashcode ...etc (Alt+insert)
+    comment/un-comment single (ctrl+/)
+    comment/un-comment multiple (ctrl+shift+/)
+     */
+}
