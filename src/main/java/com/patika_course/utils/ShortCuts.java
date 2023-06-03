dbaba27c5b482311d9aa143caeeb2454b8dbed50
GHAYETH AL MASRI <amalj6570@gmail.com>
2023-06-03 16:05:27 +0300

    ShortCuts _ Run all tests

--- src/main/java/com/patika_course/utils/ShortCuts.java
+++ src/main/java/com/patika_course/utils/ShortCuts.java
@@ -1,11 +1,15 @@
 package com.patika_course.utils;
 
+import java.io.Serializable;
+
 public class ShortCuts {
+
     /*
     open settings (ctrl+alt+s)
     doubling selected (ctrl+d)
     constructors, getters & setters, equals & hashcode ...etc (Alt+insert)
     comment/un-comment single (ctrl+/)
     comment/un-comment multiple (ctrl+shift+/)
+    Run all tests for project (ctrl+shift+f10)
      */
 }
