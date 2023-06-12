13c1be3b5b92cd56200d363dd6eff6dd0ff11c75
GHAYETH AL MASRI <amalj6570@gmail.com>
2023-06-12 06:42:24 +0300

    Thymeleaf, URLs testing

---
+++ src/main/java/com/patika_course/controller/ThymeLeafController.java
@@ -0,0 +1,32 @@
+package com.patika_course.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/thymeleaf1")
+public class ThymeLeafController {
+    // 500 server error
+    /*
+    if the url is wrong (Not registered in api urls)
+    404 not found error page will show
+
+    if the url exists however, the page contains nothing
+    500 server side error will be returned
+
+    // http://localhost:8080/thymeleaf/s_err // Unregistered URL
+    // http://localhost:8080/thymeleaf1/s_err // Registered URL
+     */
+
+    @GetMapping("/s_err")
+    public String servError(){
+        return null;
+    }
+
+    // http://localhost:8080/thymeleaf1/index_0
+    @GetMapping("/index_0")
+    public String index_0(){
+        return "index_0";
+    }
+}
