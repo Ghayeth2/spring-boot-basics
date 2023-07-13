6e2639e5ab00a974fa00c5e81381596612008303
GHAYETH AL MASRI <amalj6570@gmail.com>
2023-07-13 18:38:50 +0300

    PermaLink(SWAGGER, H2-CONSOLE, JSON EDITOR)

---
+++ src/main/java/com/patika_course/tutorials/core/utils/PermaLink.java
@@ -0,0 +1,7 @@
+package com.patika_course.tutorials.core.utils;
+
+public class PermaLink {
+    //  SWAGGER: http://localhost:8080/swagger-ui/index.html
+    //  H2-console: http://localhost:8080/h2-console/
+    //  JSON Online parser:  https://jsoneditoronline.org/#left=local.gesomi&right=local.revibe
+}
